44e5a2ecb02e33aa6a347fb2a36907fd3aa16f7c
Alvin Ng <alvinoalvin123@gmail.com>
2021-04-07 21:10:44 +0000

    client setupInput and handler

--- client.js
+++ client.js
@@ -13,15 +13,56 @@ const connect = function() {
     console.log("Successfully connected to game server");
   });
 
+  conn.write("Move: up");
+
   process.stdin.on('data', message => conn.write("Name: [ ]"));
-  // process.stdin.on('data', (key) => {
-  //   if (key === 'w') {
-  //   }
-  //   console.log(key)
-  //   process.stdout.write('.');
-  // });
+
+  conn.on('end', () =>
+    console.log('client disconnected from server')
+  );
 
   return conn;
 };
+
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const handleUserInput = (key) => {
+  if (key === '\u0003') {
+    process.exit();
+  }
+}
+const setupInput = function() {
+
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  stdin.on('data', key => {
+    handleUserInput(key)
+  })
+
+  return stdin;
+}
+setupInput();
+
+// let conn = connect()
+// conn.on('data', (key) => {
+//   if (key === 'w') {
+//     connect.write("Move: up");
+//   }
+//   if (key === 'w') {
+//     connect.write("Move: down");
+//   }
+//   if (key === 'w') {
+//     connect.write("Move: left");
+//   }
+//   if (key === 'w') {
+//     connect.write("Move: right");
+//   }
+// }
+// );
 // console.log(connect);
 module.exports = { connect };
